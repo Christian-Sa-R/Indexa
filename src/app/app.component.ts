27ccb8e6f821db0eb07ce3ab839527d03ef41ffd
Christian-Sa-R <christian.sa.1507@gmail.com>
2026-08-10 15:43:36 -0300

    primeiro curso completo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,59 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { ContainerComponent } from "./componentes/container/container.component";
-import { CabecalhoComponent } from "./componentes/cabecalho/cabecalho.component";
+import { ContainerComponent } from './componentes/container/container.component';
+import { CabecalhoComponent } from './componentes/cabecalho/cabecalho.component';
 import { SeparadorComponent } from './componentes/separador/separador.component';
+import { ContatoComponent } from './componentes/contato/contato.component';
+import { FormsModule } from '@angular/forms';
+
+interface Contato {
+  id: number;
+  nome: string;
+  telefone: string;
+}
+
+import agenda from './agenda.json';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
-    CommonModule, 
-    RouterOutlet, 
-    ContainerComponent, 
-    CabecalhoComponent, 
-    SeparadorComponent
+    CommonModule,
+    RouterOutlet,
+    ContainerComponent,
+    CabecalhoComponent,
+    SeparadorComponent,
+    ContatoComponent,
+    FormsModule,
   ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'indexa';
+  alfabeto: string = 'abcdefghijklmnopqrstuvwxyz';
+  contatos: Contato[] = agenda;
+
+  private normalizarString(texto: string): string {
+    return texto
+      .toLowerCase()
+      .normalize('NFD')
+      .replace(/[\u0300-\u036f]/g, '');
+  }
+
+  filtroPorTexto: string = '';
+
+  filtrarContatosBusca(): Contato[] {
+    return this.contatos.filter((contato) => {
+      return this.normalizarString(contato.nome).includes(
+        this.normalizarString(this.filtroPorTexto),
+      );
+    });
+  }
+
+  filtrarContatosLetraInicial(letra: string): Contato[] {
+    return this.filtrarContatosBusca().filter((contato) => {
+      return contato.nome.toLowerCase().startsWith(letra);
+    });
+  }
 }
